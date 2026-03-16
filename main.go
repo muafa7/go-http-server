95284464ea44ed648632c8e2525fdbc17faac6b3
muafa7 <muafa@LAPTOP-T5CVNTSJ.localdomain>
2026-03-16 17:46:23 +0700

    serve static files with http.FileServer

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+func main() {
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Handler: mux,
+		Addr: ":8080",
+	}
+	
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+}
